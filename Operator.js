4fdca64eb833500a00a750bea0df0d538b58ddde
Versha Dewangan <vershadewangan8@gmail.com>
2026-08-19 16:24:35 +0530

    commit

---
+++ Operator.js
@@ -0,0 +1,34 @@
+let a = 5;
+let b = 2;
+
+//Arithmetic operator
+console.log("a = ", a , " & b =", b);
+console.log(" a + b =" , a+b);
+console.log(" a - b =" , a-b);
+console.log(" a * b =" , a*b);
+console.log(" a / b =" , a/b);
+console.log(" a % b =" , a%b);
+
+//Exponentiation
+console.log(" a ** b =" , a**b);//let , 5^2=25
+
+//comparision operator
+console.log("5==2", a==b);
+console.log("5!=2", a!=b);
+console.log("6<=3" , a<=b);
+console.log("6>=3" , a>=b);
+
+// // unary operator
+//let a = 5;
+a++;
+//a--; post decrement
+console.log("a = ", a);
+// a = ++a;
+// // a = --a; pre decrement
+// console.log("a++ = ",a++)
+// console.log(" a " , a);
+
+//logical operator
+
+
+
---
+++ basic.js
@@ -0,0 +1,25 @@
+ const product = {
+  title: "Ball pen",
+  rating: 4,
+  colour: "black",
+  price: 540,
+  offer: 5,
+};
+//product["title"] = "jell pen";
+//console.log(title ); for change the name
+console.log(product);
+
+
+const  profile = {
+  username :"XYZ",
+  follower : 100 ,
+  following : 100,
+  isfollow : false,
+};
+//console.log(typeof profile["follower"]);
+console.log(profile);
+
+{
+  let x = BigInt("123");
+  let y = Symbol("Hello!");
+}
