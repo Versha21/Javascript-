1f4f405db889514e6bc64a8009e2307cae8793ae
Versha Dewangan <vershadewangan8@gmail.com>
2026-08-30 14:28:31 +0530

    update

---
+++ loop.js
@@ -0,0 +1,12 @@
+//Print 1 to 5
+// for (i = 0; i <= 5; i++) {
+//   console.log("The value of i is :" + i);
+// }
+
+//calculate the sum of first 5 numbers
+let sum = 0;
+for (i = 0 ; i <= 5 ; i++){
+  sum += i;
+}
+console.log("sum =" + sum);
+console.log("loop was ended");
