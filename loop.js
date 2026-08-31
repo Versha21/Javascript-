5a3ff99a902e695b786e0c2e8149160edef6d4f0
Versha Dewangan <vershadewangan8@gmail.com>
2026-08-31 17:13:33 +0530

    update

--- loop.js
+++ loop.js
@@ -5,8 +5,20 @@
 
 //calculate the sum of first 5 numbers
 let sum = 0;
-for (i = 0 ; i <= 5 ; i++){
+for (let i = 0 ; i <= 5 ; i++){
   sum += i;
 }
-console.log("sum =" + sum);
+console.log("sum = " + sum);
 console.log("loop was ended");
+
+// also use this for 1 10 n
+// let sum = 0;
+// let n = 5;
+// for (let i = 0 ; i <= n ; i++){
+//   sum += i;
+// }
+// console.log("sum = " + sum);
+// console.log("loop was ended");
+
+
+
