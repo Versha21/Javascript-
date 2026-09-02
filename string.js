2838f4a0d6bad5a9a47e2e0cdaa18b803b01e18e
Versha Dewangan <vershadewangan8@gmail.com>
2026-09-02 20:12:01 +0530

    update

---
+++ string.js
@@ -0,0 +1,37 @@
+//STRING
+
+// //both single and double quotes are used to define string in javascript
+let string1  = "MY javascript";
+let string2 = 'versha dewangan';
+
+console.log(string1[5]);
+console.log(string2[5]);
+
+//template literals
+let something= {
+  item: "laptop",
+  price: 50000,
+};
+
+// String interpolation
+let output = `the price of ${something.item} is ${something.price} rupees`;
+console.log(output);
+
+let specialstring = ` This is the template literal ${1+2+3}`;
+console.log(specialstring);
+console.log(typeof specialstring);
+
+//Escape Characters
+console.log("versha \n dewangan");
+console.log(`versha \t dewangan`);
+
+let myname = "versha \t dewangan";
+console.log(myname.length);
+
+// String Methods
+
+
+
+
+
+
